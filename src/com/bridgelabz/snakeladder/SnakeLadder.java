05ba3807f6b0777fee89c45a5e1149c10c624822
Rahul Bhandari <rbjainrahul@gmail.com>
2022-02-22 20:33:02 +0530

    UC1 code

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -2,4 +2,13 @@ package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
 
+	public static void main(String[] args) {
+		final int POSITION = 0;
+		final int DICE_START_RANGE = 1;
+		final int DICE_END_RANGE = 6 - DICE_START_RANGE;
+
+		int diceNumber = (int) Math.floor(Math.random() * DICE_END_RANGE) + DICE_START_RANGE;
+		System.out.println(diceNumber);
+	}
+
 }
