eb81ab280fa021dce32a6e8160c3f4023b0e2558
a-faisalkhan <kfaisalit@gmail.com>
2022-02-23 19:52:47 +0530

    initialized zero position

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -3,11 +3,15 @@ package com.bridgelabz.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		final int POSITION = 0;
+	    int position = 0;
 		final int DICE_START_RANGE = 1;
 		final int DICE_END_RANGE = 6 - DICE_START_RANGE;
 
 		int diceNumber = (int) Math.floor(Math.random() * DICE_END_RANGE) + DICE_START_RANGE;
+		int option = (int) Math.floor(Math.random() *3); 
+		if (option == 0) {
+			System.out.println("Player position is: " + position);
+		}
 		System.out.println(diceNumber);
 	}
 
