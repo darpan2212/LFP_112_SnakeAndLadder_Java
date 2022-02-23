0bfaf6f6668e72bfc761fe21cc20b3791995d9b8
Ashok Koda <kodaashok60@gmail.com>
2022-02-23 23:04:23 +0530

    Added:UC4 - If the player position moves below 0, then the player restarts from 0

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,30 +1,34 @@
 package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
+	public static final int START_POSITION = 0;
+	public static final int IS_SNAKE = 1;
+	public static final int IS_LADDER = 2;
 
 	public static void main(String[] args) {
-	    int position = 0;
+		System.out.println("Welcome to Snake And Ladder Game");
+		int position = 0;
 		final int DICE_START_RANGE = 1;
 		final int DICE_END_RANGE = 6 - DICE_START_RANGE;
 
 		int diceNumber = (int) Math.floor(Math.random() * DICE_END_RANGE) + DICE_START_RANGE;
-		int option = (int) Math.floor(Math.random() *3); 
-		if (option == 0) {
-			System.out.println("Player position is: " + position);
-		} else if ( option == 1 ) {
-			//System.out.println("Player in Ladder position : " +option);
-			
-			position = position + diceNumber;
-			
-			System.out.println("Player in Ladder position : " +position);
-
-			
-		} else if (option == 2) {
-			//System.out.println("Player in Snake position : " +option);
-			
-			position = position + diceNumber;
+		System.out.println("<--------------------------------------------->");
+		System.out.println("Dice Number : " + diceNumber);
+		int option = (int) Math.floor(Math.random() * 3);
+		System.out.println("<--------------------------------------------->");
+		if (option == IS_SNAKE) {
+			position -= diceNumber;
+			// UC-4
+			if (position < 0) {
+				position = START_POSITION;
+			}
+			System.out.println("Player in snake position : " + position);
+		} else if (option == IS_LADDER) {
+			position += diceNumber;
+			System.out.println("Player in ladder position : " + position);
+		} else {
+			System.out.println("No Play " + position);
 		}
-		System.out.println(diceNumber);
 	}
 
 }
