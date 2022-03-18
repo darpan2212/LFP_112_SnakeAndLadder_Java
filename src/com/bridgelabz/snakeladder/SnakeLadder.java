ed971b374175bad91b6cfaa068006cc206cec84b
smbiradar92 <smbiradar92@gmail.com>
2022-03-18 13:42:31 +0530

    Updating UC4 and UC5 to Snake ladder

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
-
+	
 	public static void main(String[] args) {
+
+		System.out.println(
+				"Welcome to the game of Snake and ladder");
+		
 		int position = 0;
 		final int NO_PLAY = 0;
 		final int LADDER = 1;
@@ -11,26 +15,26 @@ public class SnakeLadder {
 		final int DICE_START_RANGE = 1;
 		final int DICE_END_RANGE = 6 - DICE_START_RANGE;
 
-		int diceNumber = (int) Math
-				.floor(Math.random() * DICE_END_RANGE)
-				+ DICE_START_RANGE;
-		int option = (int) Math.floor(Math.random() * 3);
-
-		System.out.println(
-				"Welcome to the game of Snake and ladder");
-		System.out.println("You have got a Dice number of :"
-					+ diceNumber);
 		System.out.println("Your current position is :"
 					+ position);
 
-		switch (option) {
-			case NO_PLAY:
-				System.out.println(
-						"Sorry!!! You are on NO PLAY position.  Your current position "
-								+ diceNumber
-								+ " is  Please wait for your next turn");
-				position = position;
+		while(position != 100) {
 
+			int diceNumber = (int) Math
+					.floor(Math.random() * DICE_END_RANGE)
+					+ DICE_START_RANGE;
+			int option = (int) Math.floor(Math.random() * 3);
+
+			System.out.println("You have got a Dice number of :"
+						+ diceNumber);
+			switch (option) {
+			case NO_PLAY:
+				position = position;
+				
+				System.out.println(
+						"Sorry!!! You are on NO PLAY position. Your current position "
+								+ position
+								+ " Please wait for your next turn");
 				break;
 			case LADDER:
 				position = position + diceNumber;
@@ -39,18 +43,26 @@ public class SnakeLadder {
 								+ diceNumber
 								+ " numbers to reach position :"
 								+ position);
+				if(position > 100) {
+					position = position -diceNumber;
+				}
 				break;
 			case SNAKE:
+				position = position - diceNumber;
 				System.out.println(
 						"Oops!!! You are bit by a snake. You have to get back by "
 								+ diceNumber
 								+ " numbers to reach position :"
-								+ position);
-				position = position - diceNumber;
+								+ position);				
 				if (position < 0) {
 					position = position + diceNumber;
 				}
+				break;
+				}
 			}
-			diceNumber++;
+		if (position == 100) {
+			System.out.println(" Congratulations, you won!!!!! +/t+ GAME OVER");
+			System.out.println("Thankyou for playing the Game!!! Hve a nyc day");
+		}
 		}
 	}
